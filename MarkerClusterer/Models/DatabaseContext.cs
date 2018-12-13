772fb03250fa04b92c9f8c7db92f3d754ac38257
Mario Andres Botero Florez <mariobot113@hotmail.com>
2018-12-13 09:54:14 -0500

    Add migration and update database

---
+++ MarkerClusterer/Models/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MarkerClusterer.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("name=DefaultConnection") { }
+
+        public DbSet<Item> Items { get; set; }
+
+    }
+}
