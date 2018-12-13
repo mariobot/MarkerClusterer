e3494838c3faf5dc90ab78392b3adea934f55eaa
Mario Andres Botero Florez <mariobot113@hotmail.com>
2018-12-13 09:36:45 -0500

    Add ItemClass, EntityFramework and Database

---
+++ MarkerClusterer/Models/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MarkerClusterer.Models
+{
+    public class Item
+    {
+        public int id { get; set; }
+
+        public string Name { get; set; }
+
+        public type Nodo { get; set; }
+
+        public float Latitude { get; set; }
+
+        public float Longitude { get; set; }
+        
+    }
+
+    public enum type { Compañia, Bloque, Campo, Cluster, Pozos }
+    
+}
