adb953d87deb338475695c10fa20ca26ff2ec643
Mario Andres Botero Florez <mariobot113@hotmail.com>
2019-01-14 18:21:49 -0500

    Refactoring for publish

--- MarkerClusterer/Models/DatabaseContext.cs
+++ MarkerClusterer/Models/DatabaseContext.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace MarkerClusterer.Models
 {
     public class DatabaseContext : DbContext
     {
-        public DatabaseContext() : base("name=DefaultConnection") { }
+        public DatabaseContext() : base("DefaultConnection")
+        {
+            //disable initializer
+            Database.SetInitializer<DatabaseContext>(null);
+        }
 
         public DbSet<Item> Items { get; set; }
-
     }
 }
