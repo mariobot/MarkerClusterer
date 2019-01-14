cc62e74f3881f1eac7f5d7f9eb1dc2f510712a16
Mario Andres Botero Florez <mariobot113@hotmail.com>
2019-01-14 11:45:31 -0500

    Correct labels to spanish, add Configuration Controller and pages

---
+++ MarkerClusterer/Controllers/ConfigurationController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MarkerClusterer.Controllers
+{
+    public class ConfigurationController : Controller
+    {
+        public ActionResult Reports()
+        {
+            return View();
+        }
+
+        public ActionResult ParamsReports()
+        {
+            return View();
+        }
+
+        public ActionResult Alarms()
+        {
+            return View();
+        }
+
+        public ActionResult Users()
+        {
+            return View();
+        }
+
+        public ActionResult Documents()
+        {
+            return View();
+        }
+    }
+}
